6a0d1ac3de09a95f9a3b6c5e200a6822605bd700
dan sinclair <dj2@everburning.com>
2015-09-18 11:16:58 -0400

    Remove jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,6 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
-var jshint = require('gulp-jshint');
 var csslint = require('gulp-csslint');
-
-gulp.task('scripts', function() {
-  return gulp.src('src/js/**/*.js')
-      .pipe(jshint('.jshintrc'))
-      .pipe(jshint.reporter('default'));
-});
 
 gulp.task('css', function() {
   gulp.src('src/css/*.css')
@@ -16,7 +9,6 @@ gulp.task('css', function() {
 });
 
 gulp.task('server', ['scripts', 'css'], function() {
-  gulp.watch('src/js/**/*.js', ['scripts'])
   gulp.watch('src/css/*.css', ['css'])
 
   gulp.src('src')
